fd5169dcc0d101cbebcb55f7e06569c1bb2a6102
MPolygon <mstr.polygon@gmail.com>
2022-03-10 21:37:20 +0100

    Added args check

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@ using namespace std;
 
 int main(int argc, char *argv[]){
     
+    if (argc != 2) {
+        cout << "Usage <program> <file>" << endl;
+        return EXIT_FAILURE;
+    }
+
     std::cout << "Starting..." << std::endl;
 
     Parser parser(argv[1]);
