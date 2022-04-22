3c69dbc4f7638f93d7e7783bd0054bac66afe17d
AntoineSauzeau <72159603+AntoineSauzeau@users.noreply.github.com>
2022-04-22 11:13:16 +0200

    22/04/2022

--- interface.cpp
+++ interface.cpp
@@ -2,10 +2,43 @@
 
 Interface::Interface(Window *window_abstract_object) {
 
-    sf::RenderWindow(sf::VideoMode(200, 200), "Interface");
-    
+    std::cout << window_abstract_object->GetWidth() << window_abstract_object->GetHeight();
+    window = new sf::RenderWindow(sf::VideoMode(500, 500), "Interface");
+    this->window_abstract_object = window_abstract_object;
+}
+
+void Interface::TreatEvents() {
+
+    while (window->isOpen())
+    {
+
+        std::cout << "dqs";
+
+        // on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
+        sf::Event event;
+        while (window->pollEvent(event))
+        {
+            // évènement "fermeture demandée" : on ferme la fenêtre
+            if (event.type == sf::Event::Closed)
+                window->close();
+        }
+    }
 }
 
 void Interface::Draw() {
     
+    window->clear();
+
+    RecDraw(window_abstract_object);
+
+    window->display();
+}
+
+void Interface::RecDraw(Object* object) {
+
+    for(std::vector<Object*>::iterator child = object->GetChildrens()->begin(); child != object->GetChildrens()->end(); child++){
+        
+        (*child)->Draw();
+        RecDraw((*child));
+    }
 }
--- interface.hpp
+++ interface.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <iostream>
+
 #include "window.hpp"
 
 #include <SFML/Graphics.hpp>
@@ -8,8 +10,12 @@ class Interface {
 
     public:
         Interface(Window *window_abstract_object);
+
+        void TreatEvents();
+        void RecDraw(Object *object);
         void Draw();
 
     private:
         sf::RenderWindow *window;
+        Window* window_abstract_object = nullptr;
 };
